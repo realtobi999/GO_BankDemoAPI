24a3d2a7b233bec31324b4937b903cdcef225fcd
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-04-13 15:31:09 +0200

    [fix] added storage parameter to the handlers

--- src/handlers/customer_handlers.go
+++ src/handlers/customer_handlers.go
@@ -6,18 +6,18 @@ import (
 	"github.com/realtobi999/GO_BankDemoApi/src/types"
 )
 
-func CreateCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+func CreateCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger, s types.IStorage) {
 	
 }
-func IndexCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+func IndexCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger, s types.IStorage) {
 
 }
-func GetCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+func GetCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger, s types.IStorage) {
 
 }
-func UpdateCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+func UpdateCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger, s types.IStorage) {
 
 }
-func DeleteCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+func DeleteCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger, s types.IStorage) {
 
 }
