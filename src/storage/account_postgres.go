509c702af9fb0c4b0b6ee3c782f90a3911c0ad07
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-04-13 15:18:17 +0200

    [built] Account and Customer struct

--- src/storage/account_postgres.go
+++ src/storage/account_postgres.go
@@ -1,1 +1,19 @@
 package storage
+
+import (
+	"time"
+
+	"github.com/beevik/guid"
+)
+
+type Account struct {
+	ID                  guid.Guid
+	CustomerID          guid.Guid
+	Type                string
+	Balance             float64
+	Currency            string
+	Status              bool
+	OpeningDate         time.Time
+	LastTransactionDate time.Time
+	InterestRate        float64
+}
--- src/storage/customer_postgres.go
+++ src/storage/customer_postgres.go
@@ -1,1 +1,19 @@
 package storage
+
+import (
+	"time"
+
+	"github.com/beevik/guid"
+)
+
+type Customer struct {
+	ID        guid.Guid
+	FirstName string
+	LastName  string
+	Birthday  time.Time
+	Email     string
+	Phone     string
+	State     string
+	Address   string
+	Accounts  []Account
+}
