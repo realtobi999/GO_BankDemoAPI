dde13cc048ba6c47f3fdbec247aaf2602482ded2
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-04-13 15:28:11 +0200

    [built] basic api customer endpoints and their not implemented handlers

---
+++ src/handlers/customer_handlers.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/realtobi999/GO_BankDemoApi/src/types"
+)
+
+func CreateCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+	
+}
+func IndexCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+
+}
+func GetCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+
+}
+func UpdateCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+
+}
+func DeleteCustomerHandler(w http.ResponseWriter, r *http.Request, l types.ILogger) {
+
+}
