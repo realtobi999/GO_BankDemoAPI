279d7bb5f7ccb100ff7e9c8683becb22045ba1ec
Tobiáš Filgas <tobiasfilgas@gmail.com>
2024-04-13 12:58:55 +0200

    [chore] clearing terminal when starting the server and printing ASCII

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"log"
 
 	"github.com/realtobi999/GO_BankDemoApi/src/api"
+	u "github.com/realtobi999/GO_BankDemoApi/src/utils"
 )
 
 const port int = 8080;
 
 func main() {
+	u.ClearConsole()
+	u.PrintASCII()
+
 	server := api.NewServer(port)
 
 	log.Printf("[*] - Server successfully started on port : %v\n", server.Port)
---
+++ src/utils/console.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+func ClearConsole() {
+	// Clearing the terminal screen based on the OS
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		// For Unix-like systems
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		// For Windows
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		// Unsupported OS
+		fmt.Println("Unsupported OS. Cannot clear the terminal.")
+	}
+}
+
+/***
+ *    ______                _      _____              _                    
+ *    | ___ \              | |    /  ___|            | |                   
+ *    | |_/ /  __ _  _ __  | | __ \ `--.  _   _  ___ | |_   ___  _ __ ___  
+ *    | ___ \ / _` || '_ \ | |/ /  `--. \| | | |/ __|| __| / _ \| '_ ` _ \ 
+ *    | |_/ /| (_| || | | ||   <  /\__/ /| |_| |\__ \| |_ |  __/| | | | | |
+ *    \____/  \__,_||_| |_||_|\_\ \____/  \__, ||___/ \__| \___||_| |_| |_|
+ *                                         __/ |                           
+ *                                        |___/                            
+ */
+func PrintASCII() {
+	fmt.Println(``)
+	fmt.Println(` ______                _      _____              _                    `)
+	fmt.Println(` | ___ \              | |    /  ___|            | |                   `)
+	fmt.Println(` | |_/ /  __ _  _ __  | | __ \ '--.  _   _  ___ | |_   ___  _ __ ___  `)
+	fmt.Println(` | ___ \ / _' || '_ \ | |/ /  '--. \| | | |/ __|| __| / _ \| '_ ' _ \ `)
+	fmt.Println(` | |_/ /| (_| || | | ||   <  /\__/ /| |_| |\__ \| |_ |  __/| | | | | |`)
+	fmt.Println(` \____/  \__,_||_| |_||_|\_\ \____/  \__, ||___/ \__| \___||_| |_| |_|`)
+	fmt.Println(`                                      __/ |                           `)
+	fmt.Println(`                                     |___/                            `)
+	fmt.Println(``)
+}
