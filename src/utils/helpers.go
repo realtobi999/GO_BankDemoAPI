419c3d849b3bdc7302a9361070a47cb9a0d7391a
realtobi999 <tobiasfilgas@gmail.com>
2024-04-15 08:10:27 +0200

    [feat] added helper function to decode the body into a struct

--- src/utils/helpers.go
+++ src/utils/helpers.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -44,4 +46,12 @@ func ParseLimitOffsetParams(r *http.Request) (int, int, error) {
 	}
 
 	return limit, offset, nil
+}
+
+func Decode[T any](r *http.Request) (T, error) {
+	var v T
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return v, fmt.Errorf("decode json: %w", err)
+	}
+	return v, nil
 }
