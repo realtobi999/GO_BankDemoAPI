faebc04fb7eaeb65d3b57a1108e0bab47d72b10e
realtobi999 <tobiasfilgas@gmail.com>
2024-04-17 17:07:34 +0200

    [built] accounts table migrations

---
+++ src/storage/migrations/002_create_accounts_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id UUID PRIMARY KEY,
+    customer_id UUID NOT NULL,
+    balance FLOAT,
+    account_type VARCHAR(255),
+    currency VARCHAR(255),
+    status BOOLEAN,
+    opening_date TIMESTAMP WITH TIME ZONE,
+    last_transaction_date TIMESTAMP WITH TIME ZONE,
+    interest_rate FLOAT
+);
