171f75dc502be6b7773949a69c28b55e60676c8e
realtobi999 <tobiasfilgas@gmail.com>
2024-04-24 15:27:16 +0200

    [built] test for getting all transactions endpoint

--- tests/transaction_test.go
+++ tests/transaction_test.go
@@ -14,15 +14,96 @@ import (
 	"github.com/realtobi999/GO_BankDemoApi/src/core/domain"
 )
 
-func Test_Transactions_Create_Works(t *testing.T) {
+func Test_Transaction_GetAll_Works(t *testing.T) {
 	customer1 := NewTestCustomer()
 	customer2 := NewTestCustomer()
 
 	senderAcc := NewTestAccount(customer1.ID)
 	receiverAcc := NewTestAccount(customer2.ID)
 
-	senderAcc.Balance = 1000;
-	receiverAcc.Currency = "EUR";
+	transaction1 := NewTestTransaction(senderAcc.ID, receiverAcc.ID)
+	transaction2 := NewTestTransaction(senderAcc.ID, receiverAcc.ID)
+	transaction3 := NewTestTransaction(senderAcc.ID, receiverAcc.ID)
+	transaction4 := NewTestTransaction(senderAcc.ID, receiverAcc.ID)
+	transaction5 := NewTestTransaction(senderAcc.ID, receiverAcc.ID)
+
+	db := NewTestDatabase()
+	server := NewTestServer(db)
+
+	db.CreateCustomer(customer1)
+	db.CreateCustomer(customer1)
+	db.CreateAccount(senderAcc)
+	db.CreateAccount(receiverAcc)
+
+	db.CreateTransaction(transaction1)
+	db.CreateTransaction(transaction2)
+	db.CreateTransaction(transaction3)
+	db.CreateTransaction(transaction4)
+	db.CreateTransaction(transaction5)
+
+	req, err := http.NewRequest("GET", "/api/transactions", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(handlers.NewTransactionHandler(server.TransactionService).Index)
+	handler.ServeHTTP(recorder, req)
+
+	assertEqual(t, http.StatusOK, recorder.Code)
+
+	body := struct {
+		Message string                  `json:"message"`
+		Code    int                     `json:"code"`
+		Data    []domain.TransactionDTO `json:"data"`
+	}{}
+	if err := json.NewDecoder(recorder.Body).Decode(&body); err != nil {
+		t.Fatal(err)
+	}
+
+	assertEqual(t, transaction1.ID.String(), body.Data[0].ID.String())	
+	assertEqual(t, transaction2.ID.String(), body.Data[1].ID.String())	
+	assertEqual(t, transaction3.ID.String(), body.Data[2].ID.String())	
+	assertEqual(t, transaction4.ID.String(), body.Data[3].ID.String())	
+	assertEqual(t, transaction5.ID.String(), body.Data[4].ID.String())	
+}
+
+func Test_Transaction_GetAll_GivesErrorWhenNothingFound(t *testing.T) {
+	db := NewTestDatabase()
+	server := NewTestServer(db)
+
+	req, err := http.NewRequest("GET", "/api/transactions", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	recorder := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(handlers.NewTransactionHandler(server.TransactionService).Index)
+	handler.ServeHTTP(recorder, req)
+
+	assertEqual(t, http.StatusNotFound, recorder.Code)
+
+	body := struct {
+		ErrorMessage string                  `json:"error_message"`
+		Code    int                     `json:"code"`
+	}{}
+	if err := json.NewDecoder(recorder.Body).Decode(&body); err != nil {
+		t.Fatal(err)
+	}
+
+	assertEqual(t, "Error not found: Transactions not found", body.ErrorMessage)
+}
+
+
+func Test_Transaction_Create_Works(t *testing.T) {
+	customer1 := NewTestCustomer()
+	customer2 := NewTestCustomer()
+
+	senderAcc := NewTestAccount(customer1.ID)
+	receiverAcc := NewTestAccount(customer2.ID)
+
+	senderAcc.Balance = 1000
+	receiverAcc.Currency = "EUR"
 
 	db := NewTestDatabase()
 	server := NewTestServer(db)
@@ -38,25 +119,25 @@ func Test_Transactions_Create_Works(t *testing.T) {
  	 	"Amount": 100,
 	  	"Currency": "USD"
 	}
-	`, receiverAcc.ID.String())							
+	`, receiverAcc.ID.String())
 
 	url := fmt.Sprintf("/api/%s/account/%s/transaction", customer1.ID.String(), senderAcc.ID.String())
 
-	req, err := http.NewRequest("POST", url, strings.NewReader(body))									
+	req, err := http.NewRequest("POST", url, strings.NewReader(body))
 	if err != nil {
 		t.Fatal(err)
 	}
 	recorder := httptest.NewRecorder()
 
-	router := chi.NewRouter()	
+	router := chi.NewRouter()
 	router.Post("/api/{customer_id}/account/{account_id}/transaction", handlers.NewTransactionHandler(server.TransactionService).Create)
 	router.ServeHTTP(recorder, req)
 
 	assertEqual(t, http.StatusCreated, recorder.Code)
 
-	idStr := recorder.Header().Get("Location")[strings.Index(recorder.Header().Get("Location"), "/api/transaction/")+ len("/api/transaction/"):]
+	idStr := recorder.Header().Get("Location")[strings.Index(recorder.Header().Get("Location"), "/api/transaction/")+len("/api/transaction/"):]
 	assertDatabaseHas(t, "transactions", "id", idStr, db)
-	
+
 	id, err := uuid.Parse(idStr)
 	if err != nil {
 		t.Fatal(err)
@@ -67,18 +148,18 @@ func Test_Transactions_Create_Works(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	assertEqual(t, senderAcc.ID.String(), transaction.SenderAccountID.String())	
+	assertEqual(t, senderAcc.ID.String(), transaction.SenderAccountID.String())
 	assertEqual(t, receiverAcc.ID.String(), transaction.ReceiverAccountID.String())
 
 	assertEqual(t, domain.Currency("USD"), transaction.CurrencyPair.From)
 	assertEqual(t, receiverAcc.Currency, transaction.CurrencyPair.To)
 
 	assertDatabaseHas(t, "accounts", "balance", domain.ConversionRateMap[transaction.CurrencyPair]*transaction.Amount, db)
-	assertDatabaseHas(t, "accounts", "balance", senderAcc.Balance - transaction.Amount, db)
+	assertDatabaseHas(t, "accounts", "balance", senderAcc.Balance-transaction.Amount, db)
 }
 
 func Test_Transaction_Create_GivesErrorWhenSenderDoesntHaveEnoughBalance(t *testing.T) {
-	customer1 := NewTestCustomer()	
+	customer1 := NewTestCustomer()
 	customer2 := NewTestCustomer()
 
 	sender := NewTestAccount(customer1.ID)
@@ -87,7 +168,7 @@ func Test_Transaction_Create_GivesErrorWhenSenderDoesntHaveEnoughBalance(t *test
 	db := NewTestDatabase()
 	server := NewTestServer(db)
 
-	db.CreateCustomer(customer1)	
+	db.CreateCustomer(customer1)
 	db.CreateCustomer(customer2)
 	db.CreateAccount(sender)
 	db.CreateAccount(receiver)
@@ -100,7 +181,7 @@ func Test_Transaction_Create_GivesErrorWhenSenderDoesntHaveEnoughBalance(t *test
  	 	"Amount": 1000000000,
 	  	"Currency": "USD"
 	}
-	`, receiver.ID.String())							
+	`, receiver.ID.String())
 
 	url := fmt.Sprintf("/api/%s/account/%s/transaction", customer1.ID.String(), sender.ID.String())
 
@@ -110,7 +191,7 @@ func Test_Transaction_Create_GivesErrorWhenSenderDoesntHaveEnoughBalance(t *test
 	}
 	recorder := httptest.NewRecorder()
 
-	router := chi.NewRouter()	
+	router := chi.NewRouter()
 	router.Post("/api/{customer_id}/account/{account_id}/transaction", handlers.NewTransactionHandler(server.TransactionService).Create)
 	router.ServeHTTP(recorder, req)
 
@@ -128,14 +209,14 @@ func Test_Transaction_Create_GivesErrorWhenSenderDoesntHaveEnoughBalance(t *test
 }
 
 func Test_Transactions_Create_GivesErrorWhenSenderAndReceiverAreTheSame(t *testing.T) {
-	customer1 := NewTestCustomer()	
+	customer1 := NewTestCustomer()
 
 	receiver := NewTestAccount(customer1.ID)
 
 	db := NewTestDatabase()
 	server := NewTestServer(db)
 
-	db.CreateCustomer(customer1)	
+	db.CreateCustomer(customer1)
 	db.CreateAccount(receiver)
 
 	body := fmt.Sprintf(`
@@ -144,7 +225,7 @@ func Test_Transactions_Create_GivesErrorWhenSenderAndReceiverAreTheSame(t *testi
  	 	"Amount": 1000000000,
 	  	"Currency": "USD"
 	}
-	`, receiver.ID.String())							
+	`, receiver.ID.String())
 
 	url := fmt.Sprintf("/api/%s/account/%s/transaction", customer1.ID.String(), receiver.ID.String())
 
@@ -154,17 +235,16 @@ func Test_Transactions_Create_GivesErrorWhenSenderAndReceiverAreTheSame(t *testi
 	}
 	recorder := httptest.NewRecorder()
 
-	router := chi.NewRouter()	
+	router := chi.NewRouter()
 	router.Post("/api/{customer_id}/account/{account_id}/transaction", handlers.NewTransactionHandler(server.TransactionService).Create)
 	router.ServeHTTP(recorder, req)
 
 	assertEqual(t, http.StatusBadRequest, recorder.Code)
 
 	rBody := struct {
-		ErrorMessage string `json:"error_message"`
-		Code         int    `json:"code"`
+		ErrorMessage string   `json:"error_message"`
+		Code         int      `json:"code"`
 		Errors       []string `json:"errors"`
-
 	}{}
 	if err := json.NewDecoder(recorder.Body).Decode(&rBody); err != nil {
 		t.Fatal(err)
